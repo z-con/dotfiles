f5f5e5587c3e626c1c08a88107112c6f9d367e60
Zach Conley <zach@zachconley.com>
2026-05-11 11:31:10 -0400

    Update dotfiles [2026-05-11 11:31]

--- .config/hypr/battery-icon.sh
+++ .config/hypr/battery-icon.sh
@@ -2,32 +2,31 @@
 BAT=$(cat /sys/class/power_supply/BAT0/capacity)
 STATUS=$(cat /sys/class/power_supply/BAT0/status)
 
-if [ "$STATUS" = "Full" ]; then
-    ICON=""
+if   [ "$STATUS" = "Full" ];     then ICON=""
 elif [ "$STATUS" = "Charging" ]; then
-    if   [ $BAT -ge 90 ]; then ICON="󰂅"
-    elif [ $BAT -ge 80 ]; then ICON="󰂋"
-    elif [ $BAT -ge 70 ]; then ICON="󰂊"
-    elif [ $BAT -ge 60 ]; then ICON="󰢞"
-    elif [ $BAT -ge 50 ]; then ICON="󰂉"
-    elif [ $BAT -ge 40 ]; then ICON="󰢝"
-    elif [ $BAT -ge 30 ]; then ICON="󰂈"
-    elif [ $BAT -ge 20 ]; then ICON="󰂇"
-    elif [ $BAT -ge 10 ]; then ICON="󰂆"
-    else                       ICON="󰢜"
-    fi
+  if   [ $BAT -ge 90 ]; then ICON="󰂅"
+  elif [ $BAT -ge 80 ]; then ICON="󰂋"
+  elif [ $BAT -ge 70 ]; then ICON="󰂊"
+  elif [ $BAT -ge 60 ]; then ICON="󰢞"
+  elif [ $BAT -ge 50 ]; then ICON="󰂉"
+  elif [ $BAT -ge 40 ]; then ICON="󰢝"
+  elif [ $BAT -ge 30 ]; then ICON="󰂈"
+  elif [ $BAT -ge 20 ]; then ICON="󰂇"
+  elif [ $BAT -ge 10 ]; then ICON="󰂆"
+  else                       ICON="󰢜"
+  fi
 else
-    if   [ $BAT -ge 90 ]; then ICON="󰁹"
-    elif [ $BAT -ge 80 ]; then ICON="󰂂"
-    elif [ $BAT -ge 70 ]; then ICON="󰂁"
-    elif [ $BAT -ge 60 ]; then ICON="󰂀"
-    elif [ $BAT -ge 50 ]; then ICON="󰁿"
-    elif [ $BAT -ge 40 ]; then ICON="󰁾"
-    elif [ $BAT -ge 30 ]; then ICON="󰁽"
-    elif [ $BAT -ge 20 ]; then ICON="󰁼"
-    elif [ $BAT -ge 10 ]; then ICON="󰁻"
-    else                       ICON="󰁺"
-    fi
+  if   [ $BAT -ge 90 ]; then ICON="󰁹"
+  elif [ $BAT -ge 80 ]; then ICON="󰂂"
+  elif [ $BAT -ge 70 ]; then ICON="󰂁"
+  elif [ $BAT -ge 60 ]; then ICON="󰂀"
+  elif [ $BAT -ge 50 ]; then ICON="󰁿"
+  elif [ $BAT -ge 40 ]; then ICON="󰁾"
+  elif [ $BAT -ge 30 ]; then ICON="󰁽"
+  elif [ $BAT -ge 20 ]; then ICON="󰁼"
+  elif [ $BAT -ge 10 ]; then ICON="󰁻"
+  else                       ICON="󰁺"
+  fi
 fi
 
 echo "$ICON $BAT%"
