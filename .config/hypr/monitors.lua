5f51ea9d9993a8767002e42071380ced4f4f6198
Zach Conley <zach@zachconley.com>
2026-08-18 15:32:25 -0400

    hypr, kitty, omarchy: update kitty.conf, monitors.lua, shell.toml [2026-08-18 15:32]

--- .config/hypr/monitors.lua
+++ .config/hypr/monitors.lua
@@ -5,4 +5,4 @@ hl.env("GDK_SCALE", "1")
 
 hl.monitor({ output = "eDP-1", mode = "2160x1350@60", position = "0x405", scale = 1 })
 hl.monitor({ output = "DP-3", mode = "3840x2160@60", position = "2160x0", scale = 1 })
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1 })
+hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1.25 })
